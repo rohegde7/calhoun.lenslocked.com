605bf132aa916abdc191a6a9c4fb2b62a09a2fee
rohegde7 <hegde.rohit7@gmail.com>
2020-12-01 03:20:59 +0530

    Have setup a basic server with / path and added content-type to the response header which is set to text/html

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello")
+	http.HandleFunc("/", handleSlash)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleSlash(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	fmt.Fprintf(w, "<h1>My name is Rohit</h1>")
 }
